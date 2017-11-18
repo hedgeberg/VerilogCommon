e697ff05c4e910dad766275d2cb3aee82bef7468
W Hedgeberg <hedgeberg@gmail.com>
2017-11-18 17:10:58 -0500

    Updated ram.v to be a 2-port RAM instead of a 1-port RAM, due to vivado issues in handling ram i/o correctly. Still follows w/e priority convention to prevent dangerous race conditions.

--- ram.v
+++ ram.v
@@ -1,17 +1,18 @@
 //`include "common/ts_buf.v"
 
-module simple_ram(mem_bus, address, r_e, w_e, clk);
+module simple_ram(mem_bus_in, mem_bus_out, address, r_e, w_e, clk);
 	//simple robust (i think?) parametrized RAM
 	//all values are parametrizable
 	parameter BYTE_W = 8;
 	parameter ADDR_W = 8;
 	parameter NUM_ADDR = 1 << ADDR_W;
 
-	inout [BYTE_W - 1:0] mem_bus;
+	input [BYTE_W - 1:0] mem_bus_in;
+	output reg [BYTE_W - 1:0] mem_bus_out;
 	input [ADDR_W - 1:0] address;
 	input r_e, w_e;//read_enable + write_enable
 	input clk;
-	
+	tri [BYTE_W - 1: 0] mem_bus;
 	reg [BYTE_W - 1:0] mem_storage [0:NUM_ADDR - 1];
 
 /*	
@@ -29,17 +30,18 @@ module simple_ram(mem_bus, address, r_e, w_e, clk);
 
 
 	//in/out buffering
+	/*
 	reg [BYTE_W-1:0] read_line;
 	wire  [BYTE_W-1:0] write_line;
 	ts_buf #(BYTE_W) buf_writes(mem_bus, write_line, w_e),
 				buf_reads(read_line, mem_bus, (r_e & ~w_e));
-
+	*/
 
 	//read/write logic
 	always @(posedge clk) begin
-		if(w_e) mem_storage[address] <= write_line;
-		else if(r_e) read_line <= mem_storage[address];
-		else read_line <= 0;
+		if(w_e) mem_storage[address] <= mem_bus_in;
+		else if(r_e) mem_bus_out <= mem_storage[address];
+		else mem_bus_out <= 0;
 	end
 	
 endmodule // simple_memory
